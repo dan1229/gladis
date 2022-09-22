799e0dd64b98e438ea48ef6d367a81a17b46086a
Daniel Nazarian <danieln@mercury.com>
2022-09-22 18:50:47 -0400

    basic model and refactoring

--- gladis/webhooks/models.py
+++ gladis/webhooks/models.py
@@ -25,86 +25,129 @@ class WebhookReceived(AbstractBaseModel):
     def get_property_action(self):
         return self.payload.get("action")
 
+
+class GithubWebhookReceiver(WebhookReceived):
+    action = models.CharField(max_length=255, help_text="The action of the event.")
+    
+    pull_request = models.JSONField(default=None, null=True)
+    workflow_run = models.JSONField(default=None, null=True)
+    workflow_job = models.JSONField(default=None, null=True)
+    
     def process_github_webhook(self, send_slack_message=True):
         slack_message = ''
+        action = self.payload.get("action")
+        self.action = action
+        self.save()
+        
         # get pr information
         if self.payload.get("pull_request"):
-            action = self.payload.get("action")
-            if action and action == "opened":
-                slack_message += "New PR opened! :tada:\n"
-            elif action and action == "closed":
-                slack_message += "PR closed!"
-            slack_message += f"\taction: {action}\n"
-            # TODO handle draft statuses
-
-            title = self.payload.get("pull_request", {}).get("title")
-            if title:
-                slack_message += f"\ttitle: {title}\n"
-
-            pr_number = self.payload.get("pull_request", {}).get("number")
-            if pr_number:
-                slack_message += f"\tpr number: {pr_number}\n"
-
-            state = self.payload.get("pull_request", {}).get("state")
-            if state:
-                slack_message += f"\tstate: {state}\n"
-
-            is_draft = self.payload.get("pull_request", {}).get("draft")
-            is_draft = str_to_bool(is_draft)
-            slack_message += f"\tis draft: {is_draft}\n"
-
-            github_user = (
-                self.payload.get("pull_request", {}).get("user", {}).get("login")
-            )
-            github_user_link = (
-                self.payload.get("pull_request", {}).get("user", {}).get("html_url")
-            )
-            if github_user:
-                slack_message += f"\tgithub user: {github_user}\n"
-                slack_message += f"\tgithub user link: {github_user_link}\n"
-
-            repository = self.payload.get("repository", {}).get("full_name")
-            repository_link = self.payload.get("repository", {}).get("html_url")
-            if repository:
-                slack_message += f"\trepository: {repository}\n"
-                slack_message += f"\trepository link: {repository_link}\n"
-
-            merged = self.payload.get("pull_request", {}).get("merged")
-            merged = str_to_bool(merged)
-            slack_message += f"\tmerged: {merged}\n"
-
-            # TODO handle if switching base branch notification?
-            
+            # update object
+            self.pull_request = self.payload.get("pull_request")
+            slack_message = self.parse_pull_request()
+            self.save()
 
         # get workflow run information
         if self.payload.get("workflow_run"):
-            action = self.payload.get("action")
-            if action and action == "completed":
-                slack_message += "\tWorkflow completed! :tada:\n"
-            elif action and action == "requested":
-                slack_message += "\tWorkflow requested! :tada:\n"
-
-            workflow_name = self.payload.get("workflow", {}).get("name")
-            workflow_state = self.payload.get("workflow_run", {}).get("state")
-            workflow_url = self.payload.get("workflow", {}).get("html_url")
-            slack_message += f"\tworkflow name: {workflow_name}\n"
-            slack_message += f"\tworkflow state: {workflow_state}\n"
-            slack_message += f"\tworkflow url: {workflow_url}\n"
+            self.workflow_run = self.payload.get("workflow_run")
+            slack_message = self.parse_workflow_run()
+            self.save()
 
         # get workflow job information
         if self.payload.get("workflow_job"):
-            action = self.payload.get("action")
-            if action and action == "completed":
-                slack_message += "\tWorkflow completed! :tada:\n"
-            elif action and action == "requested":
-                slack_message += "\tWorkflow requested! :tada:\n"
-
-            workflow_name = self.payload.get("workflow_job", {}).get("name")
-            workflow_status = self.payload.get("workflow_job", {}).get("status")
-            workflow_url = self.payload.get("workflow_job", {}).get("html_url")
-            slack_message += f"\tworkflow name: {workflow_name}\n"
-            slack_message += f"\tworkflow status: {workflow_status}\n"
-            slack_message += f"\tworkflow url: {workflow_url}\n"
+            self.workflow_job = self.payload.get("workflow_job")
+            slack_message = self.parse_workflow_job()
+            self.save()
             
         if send_slack_message:
             Slack.send_slack_message(slack_message)
+
+
+    def parse_pull_request(self):
+        slack_message = ""
+        
+        action = self.payload.get("action")
+        if action and action == "opened":
+            slack_message += "New PR opened! :tada:\n"
+        elif action and action == "closed":
+            slack_message += "PR closed!"
+        slack_message += f"\taction: {action}\n"
+        # TODO handle draft statuses
+
+        title = self.payload.get("pull_request", {}).get("title")
+        if title:
+            slack_message += f"\ttitle: {title}\n"
+
+        pr_number = self.payload.get("pull_request", {}).get("number")
+        if pr_number:
+            slack_message += f"\tpr number: {pr_number}\n"
+
+        state = self.payload.get("pull_request", {}).get("state")
+        if state:
+            slack_message += f"\tstate: {state}\n"
+
+        is_draft = self.payload.get("pull_request", {}).get("draft")
+        is_draft = str_to_bool(is_draft)
+        slack_message += f"\tis draft: {is_draft}\n"
+
+        github_user = (
+            self.payload.get("pull_request", {}).get("user", {}).get("login")
+        )
+        github_user_link = (
+            self.payload.get("pull_request", {}).get("user", {}).get("html_url")
+        )
+        if github_user:
+            slack_message += f"\tgithub user: {github_user}\n"
+            slack_message += f"\tgithub user link: {github_user_link}\n"
+
+        repository = self.payload.get("repository", {}).get("full_name")
+        repository_link = self.payload.get("repository", {}).get("html_url")
+        if repository:
+            slack_message += f"\trepository: {repository}\n"
+            slack_message += f"\trepository link: {repository_link}\n"
+
+        merged = self.payload.get("pull_request", {}).get("merged")
+        merged = str_to_bool(merged)
+        slack_message += f"\tmerged: {merged}\n"
+
+        # TODO
+        # save reviewer and author info
+        # handle if switching base branch notification?
+        
+        return slack_message
+    
+    
+    def parse_workflow_run(self):
+        slack_message = ''
+        
+        action = self.payload.get("action")
+        if action and action == "completed":
+            slack_message += "\tWorkflow completed! :tada:\n"
+        elif action and action == "requested":
+            slack_message += "\tWorkflow requested! :tada:\n"
+
+        workflow_name = self.payload.get("workflow", {}).get("name")
+        workflow_state = self.payload.get("workflow_run", {}).get("state")
+        workflow_url = self.payload.get("workflow", {}).get("html_url")
+        slack_message += f"\tworkflow name: {workflow_name}\n"
+        slack_message += f"\tworkflow state: {workflow_state}\n"
+        slack_message += f"\tworkflow url: {workflow_url}\n"
+        
+        return slack_message
+    
+    def parse_workflow_job(self):
+        slack_message = ''
+        
+        action = self.payload.get("action")
+        if action and action == "completed":
+            slack_message += "\tWorkflow completed! :tada:\n"
+        elif action and action == "requested":
+            slack_message += "\tWorkflow requested! :tada:\n"
+
+        workflow_name = self.payload.get("workflow_job", {}).get("name")
+        workflow_status = self.payload.get("workflow_job", {}).get("status")
+        workflow_url = self.payload.get("workflow_job", {}).get("html_url")
+        slack_message += f"\tworkflow name: {workflow_name}\n"
+        slack_message += f"\tworkflow status: {workflow_status}\n"
+        slack_message += f"\tworkflow url: {workflow_url}\n"
+        
+        return slack_message
