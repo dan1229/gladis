ce7a470ea8f42f5737abbe0254572a07439144cb
Daniel Nazarian <danieln@mercury.com>
2022-09-22 19:07:33 -0400

    staticmethod

--- gladis/webhooks/slack.py
+++ gladis/webhooks/slack.py
@@ -6,13 +6,14 @@ from django.conf import settings
 class SlackClient:
     client = WebClient(token=settings.SLACK_API_KEY)
 
-    def add_to_slack_string(self, slack_string, addition, new_line=True):
+    @staticmethod
+    def add_to_slack_string(slack_string: str, addition: str, new_line=True):
         """add a string to a slack message string"""
         if new_line:
             return f"{slack_string}\n{addition}"
         return f"{slack_string}{addition}"
 
-    def send_slack_message(self, message, slack_channel=None, slack_mention=None):
+    def send_slack_message(self, message: str, slack_channel=None, slack_mention=None):
         """_send slack message to a channel"""
         if settings.SLACK_CHANNEL_OVERRIDE or not slack_channel:
             slack_channel = settings.SLACK_CHANNEL_OVERRIDE
@@ -23,7 +24,7 @@ class SlackClient:
         try:
             response = self.client.chat_postMessage(
                 channel=f"#{slack_channel}",
-                text=f"@{slack_mention}\n---------------\n{message}",
+                text=f"<@{slack_mention}>\n---------------\n{message}",
             )
             print(response)
         except SlackApiError as e:
