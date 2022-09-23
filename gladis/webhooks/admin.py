fbae4544b84579789481f974ea87613cb7216d78
Daniel Nazarian <danieln@mercury.com>
2022-09-23 16:13:45 -0400

    admins

--- gladis/webhooks/admin.py
+++ gladis/webhooks/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from webhooks.models import WebhookReceived, GithubWebhookReceived
+from webhooks.models import WebhookReceived, GithubWebhookReceived, GithubPullRequest, GithubWorkflow
 
 
 class WebhookReceivedAdmin(admin.ModelAdmin):
@@ -37,6 +37,56 @@ class GithubWebhookReceivedAdmin(admin.ModelAdmin):
     readonly_fields = ("id",)
     list_per_page = 100
 
+class GithubPullRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "pr_number",
+        "repository",
+        "github_user",
+        "state",
+        "is_merged",
+        "is_draft",
+        "datetime_created",
+    )
+    search_fields = (
+        "title",
+        "pr_number",
+        "repository",
+        "github_user",
+        "state",
+        "is_merged",
+        "is_draft",
+        "datetime_created",
+    )
+    readonly_fields = ("id",)
+    list_per_page = 100
 
+class GithubWorkflowAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "github_id",
+        "name",
+        "action",
+        "status",
+        "conclusion",
+        "pull_request_id",
+        "datetime_created",
+    )
+    search_fields = (
+        "title",
+        "github_id",
+        "name",
+        "action",
+        "status",
+        "conclusion",
+        "pull_request_id",
+        "datetime_created",
+    )
+    readonly_fields = ("id",)
+    list_per_page = 100
+    
+    
 admin.site.register(WebhookReceived, WebhookReceivedAdmin)
 admin.site.register(GithubWebhookReceived, GithubWebhookReceivedAdmin)
+admin.site.register(GithubPullRequest, GithubPullRequestAdmin)
+admin.site.register(GithubWorkflow, GithubWorkflowAdmin)
--- gladis/webhooks/models.py
+++ gladis/webhooks/models.py
@@ -87,37 +87,36 @@ class GithubWebhookReceived(WebhookReceived):
             self.save()
 
 
-
 #
 # GITHUB PULL REQUEST ====================================== #
 #
 class GithubPullRequest(AbstractBaseModel):
     title = models.CharField(max_length=255, help_text="The title of the PR.")
     github_id = models.CharField(max_length=255, help_text="The id of the workflow.")
-    
+
     pr_number = models.IntegerField(help_text="The number of the PR.")
-    action = models.CharField(max_length=255, help_text="The action of the PR.")
     state = models.CharField(max_length=255, help_text="The state of the PR.")
-    
+
     is_draft = models.BooleanField(default=False, help_text="Is the PR a draft?")
     is_merged = models.BooleanField(default=False, help_text="Is the PR merged?")
-    
+
     github_user = models.CharField(max_length=255, help_text="The user of the PR.")
-    github_user_link = models.CharField(max_length=255, help_text="The user link of the PR.")
+    github_user_link = models.CharField(
+        max_length=255, help_text="The user link of the PR."
+    )
     repository = models.CharField(max_length=255, help_text="The repository of the PR.")
-    repository_link = models.CharField(max_length=255, help_text="The repository link of the PR.")
-    
+    repository_link = models.CharField(
+        max_length=255, help_text="The repository link of the PR."
+    )
+
     reviewers = models.JSONField(default=None, null=True)
-    
-    
+
     class Meta:
         ordering = ["datetime_created"]
 
     def __str__(self):
-        return (
-            f"PR #{self.pr_number}: {self.title} by {self.github_user}"
-        )
-        
+        return f"PR #{self.pr_number}: {self.title} by {self.github_user}"
+
 
 #
 # GITHUB WORKFLOW ====================================== #
@@ -125,18 +124,17 @@ class GithubPullRequest(AbstractBaseModel):
 class GithubWorkflow(AbstractBaseModel):
     title = models.CharField(max_length=255, help_text="The title of the workflow.")
     github_id = models.CharField(max_length=255, help_text="The ID of the workflow.")
-    
+
     action = models.CharField(max_length=255, help_text="The action of the workflow.")
     name = models.CharField(max_length=255, help_text="The name of the workflow.")
     status = models.CharField(max_length=255, help_text="The status of the PR.")
     conclusion = models.CharField(max_length=255, help_text="The conclusion of the PR.")
-    pull_request_github_id = models.CharField(max_length=255, help_text="The ID of the PR.")
-    
-    
+    pull_request_github_id = models.CharField(
+        max_length=255, help_text="The ID of the PR."
+    )
+
     class Meta:
         ordering = ["datetime_created"]
 
     def __str__(self):
-        return (
-            f"Workflow #{self.github_id}: {self.name} - {self.conclusion}"
-        )
+        return f"Workflow #{self.github_id}: {self.name} - {self.conclusion}"
